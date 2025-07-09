0b3b556ea22ce2ca628f2fb331d819e3e4f4247e
Navi8torLab <navi8torlab@gmail.com>
2025-07-09 15:11:22 -0700

    Update outputs.tf

--- eks-terraform/outputs.tf
+++ eks-terraform/outputs.tf
@@ -2,8 +2,16 @@ output "cluster_name" {
   value = module.eks.cluster_name
 }
 
-output "kubeconfig" {
-  value = module.eks.kubeconfig
+output "cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
+
+output "cluster_name" {
+  value = module.eks.cluster_name
+}
+
+output "cluster_certificate_authority_data" {
+  value = module.eks.cluster_certificate_authority_data
 }
 
 output "node_group_role_arn" {
